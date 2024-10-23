c08523d90f1fa7ca89d436e386e2139926ac71ba
kouameYao <yaojean0857@gmail.com>
2024-10-23 00:39:10 +0000

    restructure project as recommended by kamyl

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ import { ApplicationModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
 
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
